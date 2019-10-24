61da2ee171a77447817674a48aad04337150f4a2
Luana Faria <luanasfff@gmail.com>
2019-10-24 17:40:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-   [:dennis_ritchie][:languages] = "C","Assembly"
+   include [:dennis_ritchie][:languages] = "C","Assembly"
    programmer_hash
 end
